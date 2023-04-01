948d6782c4ea5038aaeb99f95d580ccdbcce30b5
Govind Parmar <0.govindparmar@gmail.com>
2023-04-01 07:41:55 -0600

    Coding style improved

--- wndVidRec/aviCom.cpp
+++ wndVidRec/aviCom.cpp
@@ -7,12 +7,28 @@ _Success_(SUCCEEDED(return))
 
 HRESULT WINAPI CloseAVI(_In_ HAVI hAvi)
 {
-	if (hAvi == NULL || hAvi == INVALID_HANDLE_VALUE) return AVIERR_BADHANDLE;
-	AVI_INFO *ai = (AVI_INFO*)hAvi;
-	if (ai->aStream) AVIStreamRelease(ai->aStream);
-	if (ai->pStCmp) AVIStreamRelease(ai->pStCmp);
-	if (ai->pStream) AVIStreamRelease(ai->pStream);
-	if (ai->pFile) AVIFileRelease(ai->pFile);
+	AVI_INFO *ai = nullptr;
+	if (NULL == hAvi || INVALID_HANDLE_VALUE == hAvi)
+	{
+		return AVIERR_BADHANDLE;
+	}
+	ai = (AVI_INFO*)hAvi;
+	if (ai->aStream)
+	{
+		AVIStreamRelease(ai->aStream);
+	}
+	if (ai->pStCmp)
+	{
+		AVIStreamRelease(ai->pStCmp);
+	}
+	if (ai->pStream)
+	{
+		AVIStreamRelease(ai->pStream);
+	}
+	if (ai->pFile)
+	{
+		AVIFileRelease(ai->pFile);
+	}
 	AVIFileExit();
 	delete ai;
 	return S_OK;
@@ -22,25 +38,25 @@ _Success_(SUCCEEDED(return))
 
 HAVI WINAPI CreateAVI(_In_z_ WCHAR *fileName, _In_ INT period)
 {
-	IAVIFile *pFile;
+	IAVIFile *pFile = nullptr;
 	HRESULT hr;
 	AVI_INFO *ai = new AVI_INFO;
 
 	AVIFileInit();
 	hr = AVIFileOpenW(&pFile, fileName, OF_WRITE | OF_CREATE, NULL);
-	if (hr != AVIERR_OK)
+	if (FAILED(hr))
 	{
 		AVIFileExit();
 		return (HAVI)INVALID_HANDLE_VALUE;
 	}
 	ai->pFile = pFile;
-
 	ai->period = period;
 	ai->aStream = NULL;
 	ai->pStream = NULL;
 	ai->pStCmp = NULL;
 	ai->nFrame = 0;
 	ai->nSample = 0;
+
 	return (HAVI)ai;
 }
 
@@ -55,15 +71,18 @@ HRESULT WINAPI AVIAddFrame(_In_ HAVI hAvi, _In_ HBITMAP hBitmap, _In_ DWORD dwRa
 	{
 		AVISTREAMINFOW asInfo;
 		ZeroMemory(&asInfo, sizeof(AVISTREAMINFOW));
+
 		asInfo.fccType = streamtypeVIDEO;
 		asInfo.fccHandler = CODEC_CHARS; // Originally 0
 		asInfo.dwScale = ai->period;
-		//asInfo.dwRate = (DWORD)24;
 		asInfo.dwRate = dwRate;
 		asInfo.dwSuggestedBufferSize = dbs.dsBmih.biSizeImage;
 		SetRect(&asInfo.rcFrame, 0, 0, dbs.dsBmih.biWidth, dbs.dsBmih.biHeight);
 		hr = AVIFileCreateStreamW(ai->pFile, &ai->pStream, &asInfo);
-		if (hr != AVIERR_OK) return hr;
+		if (FAILED(hr))
+		{
+			return hr;
+		}
 	}
 
 	if (ai->pStCmp == NULL)
@@ -73,13 +92,22 @@ HRESULT WINAPI AVIAddFrame(_In_ HAVI hAvi, _In_ HBITMAP hBitmap, _In_ DWORD dwRa
 		ZeroMemory(&acOpt, sizeof(AVICOMPRESSOPTIONS));
 		acOpt.fccHandler = CODEC_CHARS;
 		hr = AVIMakeCompressedStream(&ai->pStCmp, ai->pStream, &acOpt, NULL);
-		if (hr != AVIERR_OK) return hr;
+		if (FAILED(hr))
+		{
+			return hr;
+		}
 		hr = AVIStreamSetFormat(ai->pStCmp, 0, &dbs.dsBmih, dbs.dsBmih.biSize + dbs.dsBmih.biClrUsed*sizeof(RGBQUAD));
-		if (hr != AVIERR_OK) return hr;
+		if (FAILED(hr))
+		{
+			return hr;
+		}
 	}
 
 	hr = AVIStreamWrite(ai->pStCmp, ai->nFrame, 1, dbs.dsBm.bmBits, dbs.dsBmih.biSizeImage, AVIIF_KEYFRAME, NULL, NULL);
-	if (hr != AVIERR_OK) return hr;
+	if (FAILED(hr))
+	{
+		return hr;
+	}
 	ai->nFrame++;
 
 	return S_OK; 
@@ -106,13 +134,16 @@ HRESULT WINAPI AVISetCompressionMode(_In_ HAVI hAvi, _In_ HBITMAP hBitmap, _In_ 
 		asInfo.dwSuggestedBufferSize = dbs.dsBmih.biSizeImage;
 		SetRect(&asInfo.rcFrame, 0, 0, dbs.dsBmih.biWidth, dbs.dsBmih.biHeight);
 		hr = AVIFileCreateStreamW(ai->pFile, &ai->pStream, &asInfo);
-		if (hr != AVIERR_OK)
+		if (FAILED(hr))
 		{
 			return hr;
 		}
 	}
 
-	if (ai->pStCmp != NULL) return AVIERR_COMPRESSOR;
+	if (ai->pStCmp != NULL)
+	{
+		return AVIERR_COMPRESSOR;
+	}
 	else
 	{
 		AVICOMPRESSOPTIONS newOpt, *farOpt[1];
@@ -127,12 +158,12 @@ HRESULT WINAPI AVISetCompressionMode(_In_ HAVI hAvi, _In_ HBITMAP hBitmap, _In_ 
 		}
 		hr = AVIMakeCompressedStream(&ai->pStCmp, ai->pStream, farOpt[0], NULL);
 		AVISaveOptionsFree(1, farOpt);
-		if (hr != AVIERR_OK)
+		if (FAILED(hr))
 		{
 			return hr;
 		}
-		hr = AVIStreamSetFormat(ai->pStCmp, 0, &dbs.dsBmih, dbs.dsBmih.biSize + dbs.dsBmih.biClrUsed*sizeof(RGBQUAD));
-		if (hr != AVIERR_OK)
+		hr = AVIStreamSetFormat(ai->pStCmp, 0, &dbs.dsBmih, dbs.dsBmih.biSize + dbs.dsBmih.biClrUsed * sizeof(RGBQUAD));
+		if (FAILED(hr))
 		{
 			return hr;
 		}
